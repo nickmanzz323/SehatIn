1b31c0970fa05e281755ea3a44b41d54b1efb29a
Christian <christiantjandra33@gmail.com>
2025-04-29 21:15:18 +0700

    - Membuat Database dan fungsi databaseHelper - Membuat class userRegistrationData untuk menyimpan global variabel - Membuat RegisterGeneral (setengah jadi) untuk menguji database

--- app/src/main/java/com/example/sehatin/RegisterGeneral.java
+++ app/src/main/java/com/example/sehatin/RegisterGeneral.java
@@ -1,6 +1,11 @@
 package com.example.sehatin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,51 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterGeneral extends AppCompatActivity {
 
+    public void moveToRegisterGoal(String name, String password, String email){
+        userRegistrationData userData = new userRegistrationData();
+        userData.setName(name);
+        userData.setPassword(password);
+        userData.setEmail(email);
+
+        Intent intent = new Intent(RegisterGeneral.this, RegisterGoal.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_general);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView backButton = findViewById(R.id.prevButton);
+        Button nextButton = findViewById(R.id.saveButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterGeneral.this, RegisterOption.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText user_name = findViewById(R.id.input_Name);
+                EditText user_password = findViewById(R.id.editTextTextPassword);
+                EditText user_email = findViewById(R.id.input_Email);
+
+                String name = String.valueOf(user_name.getText());
+                String password = String.valueOf(user_password.getText());
+                String email = String.valueOf(user_email.getText());
+
+                // Test DB, hapus jika selesai
+                databaseHelper db = new databaseHelper(RegisterGeneral.this);
+                db.insertUserData(1, name, email, password, "Male","Gain weight", "Active", 170, 75, 20, 2500);
+                Intent intent = new Intent(RegisterGeneral.this, RegisterGoal.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/sehatin/databaseHelper.java
@@ -0,0 +1,68 @@
+package com.example.sehatin;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+public class databaseHelper extends SQLiteOpenHelper{
+
+    private Context context;
+    private static final String DATABASE_NAME = "SehatIn.db";
+    public static final int DATABASE_VERSION = 1;
+
+    databaseHelper(@Nullable Context context) {
+        super(context, "SehtaIn.db", null, 1);
+        this.context=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createUserDatabase = "CREATE TABLE userData (UserID INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), email VARCHAR(255), password VARCHAR(255), goal VARCHAR(50), gender VARCHAR(5), active VARCHAR(15), height INT, weight INT, age INT, calorieIntake INT);";
+        db.execSQL(createUserDatabase);
+
+        // Buat query untuk tabel baru disini
+        // Contoh:
+        // String namaTable = "query...";
+        // db.execSQL(namaTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS userData");
+        onCreate(db);
+
+        // Drop table yang dibuat disini
+        // Contoh:
+        // db.execSQL("DROP TABLE IF EXIST namaTable");
+        // onCreate(db);
+    }
+
+    public void insertUserData(int userID, String name, String email, String password, String gender, String goal, String active, int height, int weight, int age, int calorieIntake){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put("userID", userID);
+        cv.put("name", name);
+        cv.put("email", email);
+        cv.put("password", password);
+        cv.put("gender", gender);
+        cv.put("goal", goal);
+        cv.put("active", active);
+        cv.put("height", height);
+        cv.put("weight", weight);
+        cv.put("age", age);
+        cv.put("calorieIntake", calorieIntake);
+
+        long result = db.insert("userData", null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
